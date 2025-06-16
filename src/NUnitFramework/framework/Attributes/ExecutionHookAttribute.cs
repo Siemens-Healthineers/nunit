df916b8772f5846fa0403fd9b97ebae0754af73e
Rajiv Sinha <rajiv.sinha@siemens-healthineers.com>
2025-06-16 11:07:37 +0200

    Base ExecutionHookAttribute for implementing specfic hook methods

--- src/NUnitFramework/framework/Attributes/ExecutionHookAttribute.cs
+++ src/NUnitFramework/framework/Attributes/ExecutionHookAttribute.cs
@@ -1,19 +1,65 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
 using NUnit.Framework.Internal.Commands;
 
 namespace NUnit.Framework
 {
     /// <summary>
-    /// Attribute to activate execution hooks.
+    /// Abstract base attribute class for activating execution hooks.
     /// </summary>
-    public abstract class ExecutionHookAttribute : NUnitAttribute, IWrapTestMethod
+    public abstract class ExecutionHookAttribute : NUnitAttribute, IWrapTestMethod, IApplyToContext
     {
         /// <inheritdoc />
         public TestCommand Wrap(TestCommand command)
         {
             return command is HookDelegatingTestCommand ? command : new HookDelegatingTestCommand(command);
+        }
+
+        /// <inheritdoc />
+        public void ApplyToContext(TestExecutionContext context)
+        {
+            if (IsMethodOverriden(nameof(BeforeTestHook)))
+            {
+                context.ExecutionHooks.AddBeforeTestHandler((sender, eventArgs) =>
+                {
+                    BeforeTestHook(context);
+                });
+            }
+
+            if (IsMethodOverriden(nameof(AfterTestHook)))
+            {
+                context.ExecutionHooks.AddAfterTestHandler((sender, eventArgs) =>
+                {
+                    AfterTestHook(context);
+                });
+            }
+        }
+
+        private bool IsMethodOverriden(string methodName)
+        {
+            var thisMethod = GetType().GetMethod(methodName);
+            var baseMethod = typeof(ExecutionHookAttribute).GetMethod(methodName);
+            return thisMethod?.DeclaringType != baseMethod?.DeclaringType;
+        }
+
+        /// <summary>
+        /// Method that is called <b>immediately before</b> the test is executed.
+        /// Override this to implement custom logic to run before the test.
+        /// </summary>
+        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
+        public virtual void BeforeTestHook(TestExecutionContext context)
+        {
+        }
+
+        /// <summary>
+        /// Method that is called <b>immediately after</b> the test is executed.
+        /// Override this to implement custom logic to run after the test.
+        /// </summary>
+        /// <param name="context">The current <see cref="TestExecutionContext"/> for the test.</param>
+        public virtual void AfterTestHook(TestExecutionContext context)
+        {
         }
     }
 }
